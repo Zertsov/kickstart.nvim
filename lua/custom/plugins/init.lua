219e40dc6152e40bea57ebdaed2777ecd4c64d33
Mitchel Vostrez <mitch@voz.dev>
2025-04-24 13:23:57 -0500

    set jj to mimic escape

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,8 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
+
+-- Set 'jj' to mimic the escape key when in normal mode
+vim.api.nvim_set_keymap('i', 'jj', '<Esc>', { noremap = true })
+
 return {}
